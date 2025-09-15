963695984076018a9bb57e3d1c5ae5b690a337e9
Hariharan Palanivelu <hariharan_palanivelu@trimble.com>
2025-09-15 12:25:21 +0530

    WellTogether - Added a basic MAUI app for initiation

---
+++ WellTogether/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace WellTogether
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ WellTogether/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace WellTogether
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
